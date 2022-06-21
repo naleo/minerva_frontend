c9c1498af26f3028a9418bc6a74a0bf9dee24951
Naleo <naleo@naleo.me>
2022-06-21 12:10:29 -1000

    Update Registration password field to password

--- packages/frontend/src/components/Register.js
+++ packages/frontend/src/components/Register.js
@@ -169,7 +169,7 @@ const Register = () => {
                         Password
                     </label>
                     <Input
-                        type="text"
+                        type="password"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white focus:outline-none"
                         name="password"
                         value={password}
